e3768e63e096e1f1eec1a8ce76a47a8897290f3d
Harsh Ambekar <harshambekar182@gmail.com>
2026-01-18 15:08:16 +0530

    push after lecture7

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+console.log("URI:", process.env.MONGODB_URI);
+
+
 import mongoose from "mongoose";
+import {DB_NAME} from './constants.js'
+import connectDB from "./db/index.js";
+
+connectDB();
+
+
+
+
+
+
+
+
+
+/* import express from "express";
+const app = express();
+
+;( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on('error' , (error)=>{
+            console.log('ERROR:' , error);
+            throw error;
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`app is listening on porrt ${process.env.PORT}`);
+            
+        })
+    } catch (err) {
+        console.error('error' , err)
+    }
+})() */
